2b09374d2fdddfd466847a2c7fb103387be4867d
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-10 22:21:56 +0530

    Refactor.Taking condition in one variable. Add.one more condition for invalid input.

--- src/weekdayOrWeekend.java
+++ src/weekdayOrWeekend.java
@@ -4,11 +4,16 @@ public class weekdayOrWeekend {
         Scanner userInput =new Scanner(System.in);
         System.out.println("Enter the day");
         String userDayInput= userInput.nextLine();
-        if (userDayInput.equalsIgnoreCase("sunday")||userDayInput.equalsIgnoreCase("saturday")){
+        boolean daySunOrsat = userDayInput.equalsIgnoreCase("sunday") || userDayInput.equalsIgnoreCase("saturday");
+        boolean daysMonToFri=userDayInput.equalsIgnoreCase("monday")||userDayInput.equalsIgnoreCase("tuesday")||userDayInput.equalsIgnoreCase("wednesday")||userDayInput.equalsIgnoreCase("thursday")||userDayInput.equalsIgnoreCase("friday");
+        if (daySunOrsat){
             System.out.println("Weekend");
         }
-        else{
+        else if(daysMonToFri){
             System.out.println("WeekDay");
+        }
+        else{
+            System.out.println("Invalid input");
         }
     }
 }
