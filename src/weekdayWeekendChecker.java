30869fb1df5e8d42aa3d3b668cdef0553642e604
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 12:41:55 +0530

    Refactor.change the implementation of condition

--- src/weekdayWeekendChecker.java
+++ src/weekdayWeekendChecker.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class weekdayWeekendChecker {
     public static void main(String[] args) {
         String userDayInput=args[0].toLowerCase();
-        HashSet<String>days=new HashSet<String>(List.of("monday","tuesday","wednesday","thursday","Friday"));
-        boolean isDayWeekend = userDayInput.equalsIgnoreCase("sunday") || userDayInput.equalsIgnoreCase("saturday");
+        Set<String>days=new HashSet<String>(List.of("monday","tuesday","wednesday","thursday","Friday"));
+        boolean isDayWeekend = userDayInput.contains("sunday")||userDayInput.contains("saturday");
         if (isDayWeekend){
             System.out.println("Weekend");
         }
