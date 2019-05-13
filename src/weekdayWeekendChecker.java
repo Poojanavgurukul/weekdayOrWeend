a4f1ff8b80b976656d66a309815fdd03065df146
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-13 12:55:48 +0530

    Refactor.change the implementation of adding days in hashset

--- src/weekdayWeekendChecker.java
+++ src/weekdayWeekendChecker.java
@@ -2,7 +2,12 @@ import java.util.*;
 public class weekdayWeekendChecker {
     public static void main(String[] args) {
         String userDayInput=args[0].toLowerCase();
-        Set<String>days=new HashSet<String>(List.of("monday","tuesday","wednesday","thursday","Friday"));
+        Set<String>days=new HashSet<String>();
+        days.add("monday");
+        days.add("tuesday");
+        days.add("wednesday");
+        days.add("thursday");
+        days.add("friday");
         boolean isDayWeekend = userDayInput.contains("sunday")||userDayInput.contains("saturday");
         if (isDayWeekend){
             System.out.println("Weekend");
