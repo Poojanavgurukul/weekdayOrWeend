0c02fdd09c31890514e3b8f007fd99b72c067d3e
Poojanavgurukul <Pooja17@navgurukul.org>
2019-05-21 08:58:33 +0530

    Refactor. change the name of test

--- src/Test/java/daysCheckerTest/DaysCheckerTest.java
+++ src/Test/java/daysCheckerTest/DaysCheckerTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class DaysCheckerTest {
-    @Test public void dayIsWeekend(){
+    @Test public void stringIsWeekend(){
         assertTrue("day is weekend", weekdayWeekendChecker.isDayWeekend("sunday"));
     }
     @Test public void stringIsWeekday(){
